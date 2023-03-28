3cd3a7897b23cc1449d8194d9edfc02c54eb8db3
Lee Zheng <leeminz214@gmail.com>
2023-03-28 00:17:28 -0400

    structure

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template,redirect,url_for
+from picamera import PiCamera
+from datetime import datetime
+from time import sleep
+
+app = Flask(__name__)
+
+camera = PiCamera()
+
+@app.route('/gallery')
+def gallery():
+    image_list = []
+    with open('images.txt','r') as f:
+        for image in f:
+            image_list.append(image)
+            #https://flask.palletsprojects.com/en/2.2.x/patterns/javascript/
+    return render_template('home.html', image_list = image_list)
+
+@app.route('/take_pic')
+def take_photo():
+    #camera.start_preview()
+    #camera.start_recording(f'/home/pi/Pictures/{datetime.now()}.h264')
+    #sleep(3)
+    time = str(datetime.now())
+    name = f'/home/pi/coding/picamera/static/{time}.jpg'
+    camera.capture(name)
+    with open('images.txt','a') as f:
+        f.write(f'{name}\n')
+    #camera.stop_recording()
+    #camera.stop_preview()
+    return redirect(url_for('gallery'))
+
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(debug=True)
